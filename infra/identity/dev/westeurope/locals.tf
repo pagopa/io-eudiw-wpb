947bd1dac89773af56b8701975a8131c00241f44
AF <alessandro.ferlin@pagopa.it>
2024-09-16 15:19:20 +0200

    Rename folder identity/prod to identity/dev

---
+++ infra/identity/dev/westeurope/locals.tf
@@ -0,0 +1,19 @@
+locals {
+  prefix    = "io"
+  env_short = "d"
+  env       = "dev"
+  location  = "westeurope"
+  project   = "${local.prefix}-${local.env_short}"
+  domain    = "eudiw"
+
+  repo_name = "io-eudiw-wpb"
+
+  tags = {
+    CostCenter     = "TS310 - PAGAMENTI & SERVIZI"
+    CreatedBy      = "Terraform"
+    Environment    = "Dev"
+    Owner          = "IO"
+    ManagementTeam = "IO Wallet"
+    Source         = "https://github.com/pagopa/io-eudiw-wpb/blob/main/infra/identity/prod/westeurope"
+  }
+}
