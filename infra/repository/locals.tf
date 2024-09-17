bd123aabe560a3ec9144aa694c2c7519139a958f
AF <alessandro.ferlin@pagopa.it>
2024-09-17 15:33:49 +0200

    Update repository locals

--- infra/repository/locals.tf
+++ infra/repository/locals.tf
@@ -12,13 +12,13 @@ locals {
   ci = {
     secrets = {
       "ARM_CLIENT_ID" = data.azurerm_user_assigned_identity.identity_prod_ci.client_id
-    },
-    reviewers_teams = ["io-wallet", "engineering-team-cloud-eng"]
+    }
   }
 
   cd = {
     secrets = {
       "ARM_CLIENT_ID" = data.azurerm_user_assigned_identity.identity_prod_cd.client_id
-    }
+    },
+    reviewers_teams = ["io-wallet", "engineering-team-cloud-eng"]
   }
 }
