70f58110d2cf8f6e53ba2ba70998c2b058a53104
AF <alessandro.ferlin@pagopa.it>
2024-09-17 12:25:47 +0200

    Add function api

---
+++ infra/resources/modules/function_api/main.tf
@@ -0,0 +1,31 @@
+module "func_api" {
+  source = "github.com/pagopa/dx//infra/modules/azure_function_app_exposed?ref=19f4b22"
+
+  environment = {
+    prefix          = var.prefix
+    env_short       = var.env_short
+    location        = var.location
+    domain          = var.domain
+    app_name        = "api"
+    instance_number = "01"
+  }
+
+  resource_group_name = var.resource_group_name
+  health_check_path   = "/info"
+  node_version        = 20
+
+  # All async trigger shutdown initially
+  app_settings = merge(
+    local.app_settings,
+    {}
+  )
+
+  slot_app_settings = merge(
+    local.app_settings,
+    {}
+  )
+
+  tier = "standard"
+
+  tags = var.tags
+}
