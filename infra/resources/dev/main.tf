49d9c46778db86a0b2bc213bcccc91a81256a3cf
AF <4775679+datalek@users.noreply.github.com>
2024-09-18 10:07:52 +0200

    [EUDIW-101] Add main resource group (#7)

--- infra/resources/dev/main.tf
+++ infra/resources/dev/main.tf
@@ -3,20 +3,25 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.96.0"
+      version = "<= 3.108.0"
     }
-
   }
 
   backend "azurerm" {
     resource_group_name  = "terraform-state-rg"
     storage_account_name = "tfappdevio"
     container_name       = "terraform-state"
-    key                  = "io-eudiw-wpb.resources.prod.westeurope.tfstate"
+    key                  = "io-eudiw-wpb.resources.dev.westeurope.tfstate"
   }
 }
 
 provider "azurerm" {
-  features {
-  }
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "${local.project}-${local.domain}-rg-01"
+  location = local.location
+
+  tags = local.tags
 }
