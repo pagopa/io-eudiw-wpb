4de1019e554759dec0673fe9a5ef180be5694db6
AF <4775679+datalek@users.noreply.github.com>
2024-09-20 14:28:03 +0200

    [EUDIW-94, EUDIW-104] Fix function app deploy workflow (#14)

--- apps/azure-functions-api/src/config.ts
+++ apps/azure-functions-api/src/config.ts
@@ -2,13 +2,11 @@ import * as t from 'io-ts';
 import * as E from 'fp-ts/lib/Either';
 import { pipe } from 'fp-ts/lib/function';
 import * as reporters from '@pagopa/ts-commons/lib/reporters';
-import { NonEmptyString } from '@pagopa/ts-commons/lib/strings';
 import { withDefault } from '@pagopa/ts-commons/lib/types';
 import { NumberFromString } from '@pagopa/ts-commons/lib/numbers';
 
 export type Config = t.TypeOf<typeof Config>;
 export const Config = t.type({
-  APPINSIGHTS_INSTRUMENTATIONKEY: NonEmptyString,
   // Default is 10 sec timeout
   FETCH_TIMEOUT_MS: withDefault(t.string, '10000').pipe(NumberFromString),
   isProduction: t.boolean,
