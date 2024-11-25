d8feb5cda8a36e957aff46b9a865582b843e6f24
AF <4775679+datalek@users.noreply.github.com>
2024-11-25 11:14:06 +0100

    [EUDIW-178] Add platform-agnostic hardwarekey extractor (#27)

--- apps/azure-functions-api/src/adapters/azure/functions/__tests__/create-wallet-instance.test.ts
+++ apps/azure-functions-api/src/adapters/azure/functions/__tests__/create-wallet-instance.test.ts
@@ -5,6 +5,7 @@ import { CreateWalletInstanceFn } from '../create-wallet-instance';
 import { makeHttpRequest } from './data';
 import { iOSMockData } from '../../../../domain/__tests__/hardwarekey-ios/data';
 import { androidMockData } from '../../../../domain/__tests__/hardwarekey-android/data';
+import { platformAgnosticMockData } from '../../../../domain/__tests__/hardwarekey-platform-agnostic/data';
 
 describe('CreateWalletInstanceFn', () => {
   it('should return a 204 HTTP response given valid ios data', async () => {
@@ -33,6 +34,25 @@ describe('CreateWalletInstanceFn', () => {
         challenge: androidMockData.challenge,
         hardware_key_tag: androidMockData.keyId,
         key_attestation: androidMockData.attestation,
+      },
+      { 'x-user-id': 'aUserId' },
+    );
+
+    env.nonceRepository.delete.mockReturnValueOnce(TE.right(void 0));
+    env.walletInstanceRepository.insert.mockReturnValueOnce(TE.right(void 0));
+
+    const actual = await CreateWalletInstanceFn(env)(request, ctx);
+
+    expect(actual.status).toStrictEqual(204);
+  });
+
+  it('should return a 204 HTTP response given valid platform-agnostic data', async () => {
+    const { env, ctx } = makeTestEnv();
+    const request = makeHttpRequest(
+      {
+        challenge: platformAgnosticMockData.challenge,
+        hardware_key_tag: platformAgnosticMockData.keyId,
+        key_attestation: platformAgnosticMockData.attestation,
       },
       { 'x-user-id': 'aUserId' },
     );
